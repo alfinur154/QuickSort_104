42e23c37af9d2b2064ead211f6d691bfb3889fda
alfinur154 <alfinur27@gmail.com>
2023-03-30 15:32:11 +0700

    memasukkan element array dan variabel

--- QuickSort_104/QuickSort_104.cpp
+++ QuickSort_104/QuickSort_104.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 using namespace
 
 int arr[20];
@@ -18,6 +19,15 @@ void input() {
 			cout << "\nMaksimum panjang array adalah 20" << endl;
 
 	}
-}
+	cout << "\n____________________" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n____________________" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+	}	cin >> arr[i];
+}	
+
 
 
